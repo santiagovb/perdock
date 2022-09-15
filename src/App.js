b1bee7042fe4f77e34f30dd074c006e8c7d55400
Santiago <santiago@Santiagos-MacBook-Pro-2.local>
2022-09-14 20:58:57 -0500

    add/first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import button from './button'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
---
+++ src/button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './button.css'
+
+function button(){
+    return(
+        <div>
+            <button>clickme</button>
+        </div>
+    )
+}
